63018dbd2b71e643cd883e39781f1fd647ca55c6
khfdz <hafiedzdhika@gmail.com>
2024-12-09 00:23:45 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,29 @@
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect("mongodb://localhost:27017/nutech");
+//     console.log("Connected to MongoDB");
+//   } catch (err) {
+//     console.error("Error connecting to MongoDB:", err.message);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/nutech");
-    console.log("Connected to MongoDB");
+    const uri =
+      "mongodb+srv://khfdz:silvya123@cluster0.wgc9p.mongodb.net/nutech?retryWrites=true&w=majority";
+
+    await mongoose.connect(uri);
+
+    console.log("Connected to MongoDB Atlas");
   } catch (err) {
-    console.error("Error connecting to MongoDB:", err.message);
+    console.error("Error connecting to MongoDB Atlas:", err.message);
     process.exit(1);
   }
 };
